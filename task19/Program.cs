4f3e5f3071120f10a803ae5406976bdbea64a91e
Esma <mambetovaesma@gmail.com>
2022-07-18 23:18:43 +0300

    Home work 2

---
+++ task19/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+int GetPolinrome (int number) {
+    int n = number;
+    int reverse = 0;
+    while (n>0) 
+    {
+        int r = n % 10;
+        reverse = reverse * 10 + r;
+        n = n / 10;
+    }
+    if (reverse == number) 
+    {
+        return 1;
+    } else {
+        return -1;
+    }
+}
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (GetPolinrome(num) == 1) 
+{
+    Console.WriteLine("ДА");
+} else 
+{
+    Console.WriteLine("Нет");
+}
---
+++ task21/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты 
+// двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+int GetNum() 
+{   
+    int num = Convert.ToInt32(Console.ReadLine());    
+    return num;
+}
+
+double Cub(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    double result = Math.Round(Math.Sqrt(
+                                        Math.Pow(x2-x1,2) + 
+                                        Math.Pow(y2-y1,2) + 
+                                        Math.Pow(z2-z1,2)
+                                        ),
+                                        2);  
+    return result;
+}
+Console.WriteLine("Введите координаты двух точек А и В: ");
+Console.WriteLine("Введите координаты точки A: ");
+int x1 = GetNum();
+int y1 = GetNum();
+int z1 = GetNum();
+
+Console.WriteLine("Введите координаты точки B: ");
+int x2 = GetNum();
+int y2 = GetNum();
+int z2 = GetNum();
+
+Console.WriteLine(Cub(x1,y1,z1,x2,y2,z2));
+

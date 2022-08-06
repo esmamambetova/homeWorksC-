0e73fbfefba0b5ee0a204cf13c04a4597eb17ff3
Esma <mambetovaesma@gmail.com>
2022-08-06 23:17:52 +0300

    homework  25 27 29

---
+++ task25/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит 
+// число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Write("Введите число A: ");
+int num_a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int num_b = Convert.ToInt32(Console.ReadLine());
+
+int Vozvedenie_v_stepen(int num1, int num2) 
+{
+    int num = num1;
+    for (int i = 1; i < num2; i++)
+    {
+        Console.WriteLine(i);
+        num = num * num1;
+    }
+    Console.WriteLine(num);
+    return 0;
+}
+
+Vozvedenie_v_stepen(num_a, num_b);
---
+++ task27/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 27: Напишите программу, которая принимает на 
+// вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int GetSumNumbers (int number) 
+{
+    int sum = 0;
+    for (int i = 0; i < number.ToString().Count(); i++)
+    {
+        string a1 = Convert.ToString(number.ToString()[i]);
+        sum = sum + Convert.ToInt32(a1);
+    }
+    return sum;
+}
+
+Console.WriteLine($"Сумма всех чисел: {GetSumNumbers (num)}");

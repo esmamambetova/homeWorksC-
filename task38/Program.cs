be2aa96756b3098ecf2c0a4d1d82922c01e6f974
Esma <mambetovaesma@gmail.com>
2022-08-07 00:23:39 +0300

    homework lesson 5

---
+++ task38/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+int GetDiff()
+{
+    int[] mas = {3, 7, 22, 2, 78};
+    int tmp = 0;
+    
+    for (int i = 0; i < mas.Count(); i++)
+    {
+        for (int y = i+1; y < mas.Count(); y++)
+        {
+            if (mas[i]>mas[y]) 
+            {
+                tmp = mas[i];
+                mas[i] = mas[y];
+                mas[y] = tmp;
+            }
+        }
+    }
+
+    return mas[mas.Count()-1]-mas[0];
+}
+
+Console.WriteLine(GetDiff());
